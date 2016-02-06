a27aabb171690d299d0fcecc258e75900bf68ade
programmer <programmer@baconbytes.net>
2016-02-06 16:28:36 -0500

    Actual FOV of the camera?

--- 2016 Robot/src/com/explodingbacon/robot/main/HamletUtils.java
+++ 2016 Robot/src/com/explodingbacon/robot/main/HamletUtils.java
@@ -2,6 +2,6 @@ package com.explodingbacon.robot.main;
 
 public class HamletUtils {
     public double getDistanceFromPx(int sizeInPx) {
-        return 10.25*Math.atan((137/3200)*sizeInPx);
+        return 10.25*Math.tan((62.5/1280)*sizeInPx);
     }
 }
