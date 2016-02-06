f206730980ae0a940198451417e13313e9b2c96b
Ryan <weegeemus@gmail.com>
2016-02-06 15:29:30 -0500

    Commit these too please

--- 2016 Robot/src/com/explodingbacon/robot/commands/DriveCommand.java
+++ 2016 Robot/src/com/explodingbacon/robot/commands/DriveCommand.java
@@ -1,14 +1,26 @@
 package com.explodingbacon.robot.commands;
 
 import com.explodingbacon.bcnlib.framework.Command;
+import com.explodingbacon.bcnlib.framework.PIDController;
+import com.explodingbacon.bcnlib.sensors.ADXSensor;
+import com.explodingbacon.bcnlib.utils.Utils;
 import com.explodingbacon.robot.main.Robot;
 import com.explodingbacon.robot.main.OI;
 import com.explodingbacon.robot.subsystems.DriveSubsystem;
 
+import java.sql.Driver;
+
 public class DriveCommand extends Command {
 
-    public boolean left = true;
     public double deadzone = 0.08;
+
+    public double angleStart = 0;
+    public boolean buttonWasTrue = false;
+
+    public double gyroKP = 1, gyroKI = 1, min = 0.1, max = 1;
+
+    public PIDController left = new PIDController(null, DriveSubsystem.getADX(), gyroKP, gyroKI, 0, min, max);
+    public PIDController right = new PIDController(null, DriveSubsystem.getADX(), gyroKP, gyroKI, 0, min, max).setInverted(true);
 
     public DriveCommand() {
         requires(Robot.driveSubsystem);
@@ -19,37 +31,43 @@ public class DriveCommand extends Command {
 
     @Override
     public void onLoop() {
-        /*
-        double leftY = OI.left.getY();
-        double rightY = OI.right.getY();
-        leftY = Math.abs(leftY) > deadzone ? leftY : 0;
-        rightY = Math.abs(rightY) > deadzone ? rightY : 0;
-        Robot.driveSubsystem.tankDrive(leftY, rightY);
-        */
+        if (DriveSubsystem.isDriverControl()) {
+            double leftTurn = 0, rightTurn = 0;
+            if (OI.gyroForward.get()) {
+                ADXSensor adx = DriveSubsystem.getADX();
+                if (!buttonWasTrue) angleStart = adx.getAngle();
+                double angleError = adx.getAngle() - angleStart; //TODO: Check if the sign is wrong
+                if (Math.abs(angleError) > DriveSubsystem.GYRO_ANGLE_ERROR_FIX) {
+                    if (!left.isEnabled()) {
+                        left.setTarget(angleStart);
+                        left.enable();
+                    }
+                    if (!right.isEnabled()) {
+                        right.setTarget(angleStart);
+                        right.enable();
+                    }
+                    leftTurn = left.getMotorPower() / 3;
+                    rightTurn = right.getMotorPower() / 3;
+                }
+                buttonWasTrue = true;
+            } else {
+                buttonWasTrue = false;
+            }
 
-        double joyX;
-        double joyY;
-        if (OI.drive.leftJoyButton.get()) {
-            left = true;
-        } else if (OI.drive.rightJoyButton.get()) {
-            left = false;
+            double joyX;
+            double joyY;
+
+            joyX = OI.drive.getX() + leftTurn;
+            joyY = OI.drive.getY() + rightTurn;
+
+            //joyX = OI.drive.getX();
+            //joyY = OI.drive.getRawAxis(3);
+
+            joyX = Utils.deadzone(joyX, deadzone);
+            joyY = Utils.deadzone(joyY, deadzone);
+
+            DriveSubsystem.arcadeDrive(joyX, joyY);
         }
-
-        if (left) {
-            joyX = OI.drive.getX();
-            joyY = OI.drive.getY();
-        } else {
-            joyX = OI.drive.getX2();
-            joyY = OI.drive.getY2();
-        }
-
-        //joyX = OI.drive.getX();
-        //joyY = OI.drive.getRawAxis(3);
-
-        joyX = Math.abs(joyX) > deadzone ? joyX : 0;
-        joyY = Math.abs(joyY) > deadzone ? joyY : 0;
-
-        DriveSubsystem.arcadeDrive(joyX, joyY);
     }
 
     @Override
--- 2016 Robot/src/com/explodingbacon/robot/vision/VisionTargeting.java
+++ 2016 Robot/src/com/explodingbacon/robot/vision/VisionTargeting.java
@@ -6,24 +6,32 @@ import com.explodingbacon.bcnlib.utils.CodeThread;
 import com.explodingbacon.bcnlib.vision.Camera;
 import com.explodingbacon.bcnlib.vision.Contour;
 import com.explodingbacon.bcnlib.vision.Image;
+import com.explodingbacon.bcnlib.vision.Vision;
+import com.explodingbacon.robot.main.OI;
 import com.explodingbacon.robot.subsystems.DriveSubsystem;
 import com.explodingbacon.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj.vision.USBCamera;
 import java.awt.*;
 
 public class VisionTargeting extends CodeThread {
 
-    Camera camera;
-    PIDController left = null;
-    PIDController right = null;
-    InternalSource source = null;
+    private Camera camera;
+    private USBCamera usb;
+    private PIDController left = null;
+    private PIDController right = null;
+    private InternalSource source = null;
 
-    public static final double PIXELS_ERROR_FIX = 30; //The minimum pixel error needed before the Robot auto-corrects
+    public static final double PIXELS_ERROR_FIX = 50; //The minimum pixel error needed before the Robot auto-corrects
 
     double kP = 1, kI = 1, min = 0.2, max = 0.35;
 
     private void init() {
+        if (!Vision.isInitialized()) Vision.init();
         camera = new Camera(0);
         camera.getImage(); //You seem to have to call this once in order for it to work properly
+
+        usb = new USBCamera();
+        usb.setExposureManual(100); //TODO: see if this function even works
 
         source = new InternalSource();
         left = new PIDController(DriveSubsystem.getLeftMotors(), source, kP, kI, 0, min, max);
@@ -36,29 +44,52 @@ public class VisionTargeting extends CodeThread {
         super.start();
     }
 
+    //TODO: Track hits and misses and save it to a file
+
     @Override
-    public void run() {
+    public void code() {
         try {
-            Image i = camera.getImage(); //TODO: See if there is a delay from pressing shoot to actually shooting
-            int target = i.getWidth() / 2;
-            Contour goal = getGoal(i);
-            if (goal != null) {
-                double midX = goal.getMiddleX();
-                double error = Math.abs(target - midX);
-                if (error > PIXELS_ERROR_FIX && ShooterSubsystem.shouldShoot()) { //If we are off center by more than an acceptable amount of pixels, then auto-correct if we are shooting
-                    left.setTarget(target);
-                    right.setTarget(target);
-                    left.enable();
-                    right.enable();
-                    left.waitUntilDone();
-                    left.disable();
-                    right.disable();
+            Contour goal = null;
+            if (camera.isOpen()) {
+                Image i = camera.getImage(); //TODO: See if there is a delay from pressing shoot to actually shooting
+                int target = i.getWidth() / 2;
+                goal = getGoal(i);
+                if (goal != null) {
+                    double midX = goal.getMiddleX();
+                    double error = Math.abs(target - midX);
+                    if (error > PIXELS_ERROR_FIX) { //If we are off center by more than an acceptable amount of pixels, then auto-correct if we are shooting
+                        OI.drive.rumble(0, 0);
+                        if (ShooterSubsystem.shouldShoot()) {
+                            DriveSubsystem.setDriverControl(false);
+                            source.update(midX);
+                            left.setTarget(target);
+                            right.setTarget(target);
+                            left.enable();
+                            right.enable();
+                            while (!left.isDone()) { //TODO: Decide if it is safe to just wait for left or if we have to wait for right too
+                                midX = goal.getMiddleX();
+                                source.update(midX);
+                                Thread.sleep(25);
+                            }
+                            left.disable();
+                            right.disable();
+                            DriveSubsystem.setDriverControl(true);
+                        }
+                    } else { //We are lined up with the goal
+                        OI.drive.rumble(0.1f, 0.1f);
+                    }
                 }
-                if (ShooterSubsystem.shouldShoot()) {
-                    ShooterSubsystem.setRoller(1);
-                    Thread.sleep(500);
-                    ShooterSubsystem.setRoller(0);
-                    ShooterSubsystem.setShouldShoot(false);
+            }
+            //The robot will shoot the ball regardless of if it can see the target, but requires the shooter motors to be active
+            if (ShooterSubsystem.shouldShoot() && ShooterSubsystem.getEncoder().getRate() > 5) { //TODO: tweak this rate to be accurate for "shooter motors are moving at an okay speed"
+                ShooterSubsystem.setRoller(1);
+                Thread.sleep(500);
+                ShooterSubsystem.setRoller(0);
+                ShooterSubsystem.setShouldShoot(false);
+                if (!camera.isOpen()) {
+                    System.out.println("[VISION] Shooting blind due to the camera not working!");
+                } else if (goal == null) {
+                    System.out.println("[VISION] Shooting a ball despite not being able to see a goal!");
                 }
             }
         } catch (Exception e) {
