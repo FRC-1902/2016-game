5e0fe90a711c1d4ed1ced99bcb3b19ec40b193d6
programmer <programmer@baconbytes.net>
2016-02-06 16:30:54 -0500

    Oops.

--- 2016 Robot/src/com/explodingbacon/robot/main/HamletUtils.java
+++ 2016 Robot/src/com/explodingbacon/robot/main/HamletUtils.java
@@ -2,6 +2,6 @@ package com.explodingbacon.robot.main;
 
 public class HamletUtils {
     public double getDistanceFromPx(int sizeInPx) {
-        return 10.25*Math.tan((62.5/1280)*sizeInPx);
+        return 10.25/Math.tan((62.5/1280)*sizeInPx);
     }
 }
